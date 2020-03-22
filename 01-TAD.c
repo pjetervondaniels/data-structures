4f9037b408ecc6217ef31f25d0c87aec316248cb
Pedro Daniel <pedrodanieljardim@gmail.com>
2020-03-22 19:53:14 -0300

    Aula 01 - Apresentação

---
+++ 01-TAD.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  // Tipo de dado: Define o conjunto de valores (domínio) que uma
+  // variável pode assumir...
+  // por exemplo: int v = pode se assumir um conjunto dos inteiros (-2,-1,0,1,2)
+
+  // Estruturas de dados : Trata-se de um relacionamento lógico entre os tipos de dados...
+  // Tipos de estruturas de dados ou TAD incluem:
+  // - criação de estrutura;
+  // - inclusão de um elemento;
+  // - remoção de um elementos;
+  // - acesso de um elemento;
+  // - etc;
+
+  // Vantagens de usar TAD's:
+  // - Encapsulamento e Segurança: o Usuário não tem acesso direto aos dados;
+  // - Flexibilidade e Reutilização: podemos alterar o TAD sem alterar as aplicações que o ultilizam..
+
+  // Exemplos de TAD simples...
+  // Arquivos em C..
+  // FILE *f;
+  // os dados de 'f' somente podem ser acessados pelas funções de manipulação de tipo:
+  // - fopen();
+  // - fclose();
+  // - fputc();
+  // - fgetc();
+  // - feof();
+  
+
+    return 0;
+}
