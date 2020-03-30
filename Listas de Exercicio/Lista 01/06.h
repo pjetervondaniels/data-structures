76357ca8a18308de6d66220c641d955059a2e19e
Pedro Daniel <pedrodanieljardim@gmail.com>
2020-03-30 20:11:10 -0300

    att ex 05 e 06 da lista 01

---
+++ Listas de Exercicio/Lista 01/06.h
@@ -0,0 +1,9 @@
+
+typedef struct numeroRacional nr;
+
+nr* criaRacional(double x);
+
+void comparaRacional(nr* a,nr *b);
+
+double somaRacional(nr* a,nr* b);
+double multiplicaRacional(nr* a, nr*b);
