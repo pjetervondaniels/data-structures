858801b045ba6aff2b8c4128c75554d4d6c68bb6
Pedro Daniel <pedrodanieljardim@gmail.com>
2020-03-30 22:54:25 -0300

    Adicionando exercício 08 da lista 01

---
+++ Listas de Exercicio/Lista 01/08.h
@@ -0,0 +1,9 @@
+
+typedef struct cilindro cl;
+
+cl* criaCilindro(double a,double r);
+
+void tamanhoLado(cl* cil);
+void calculaArea(cl* cil);
+void calculaVolume(cl* cil);
+void liberaCubo(cl* cil);
