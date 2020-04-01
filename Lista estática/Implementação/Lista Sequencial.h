0214ecca5d5b7a9dbcf3e6c8597c4bcbf6e83674
Pedro Daniel <pedrodanieljardim@gmail.com>
2020-04-01 16:50:52 -0300

    Lista estática - Implementação

---
+++ Lista estática/Implementação/Lista Sequencial.h
@@ -0,0 +1,15 @@
+//Arquivo ListaSequencial.h
+#define MAX 100
+struct aluno{
+    int matricula;
+    char nome[30];
+    float n1,n2,n3;
+};
+
+typedef struct lista Lista;
+
+lista *li;
+
+Lista* criaLista();
+
+void liberaLista(Lista *li);
