342a78278bc302ebfe79773f384d9dccd01c5ec6
Pedro Daniel <pedrodanieljardim@gmail.com>
2020-04-04 16:22:00 -0300

    Remoção em Lista estática..

---
+++ Lista estática/Remoção/remocao.h
@@ -0,0 +1,43 @@
+/*
+    existem 3 tipos de remoção:
+    - início 
+    - meio
+    - final
+
+    também existe o caso onde a inserção é feita em uma "lista"
+    que está vazia...
+
+*/
+
+#define MAX 100
+struct aluno{
+    int matricula;
+    char nome[30];
+    float n1,n2,n3;
+};
+
+typedef struct lista Lista;
+
+Lista *li;
+
+Lista* criaLista();
+
+void liberaLista(Lista *li);
+
+int tamanhoLista(Lista* li);
+
+int listaCheia(Lista *li);
+
+int listaVazia(Lista *li);
+
+int insereFinalLista(Lista *li,struct aluno al);
+
+int insereInicioLista(Lista *li,struct aluno al);
+
+int insereListaOrdenada(Lista *li,struct aluno al);
+
+int removeFinalLista(Lista *li);
+
+int removeInicioLista(Lista *li);
+
+int removeMeioLista(Lista *li,int n);
