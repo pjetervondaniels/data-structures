ad307995bc1a25cf8c2eb22704416a42880a19ab
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-16 17:17:02 -0300

    Adiciona elemento na classe vetor

---
+++ Groner/AdicionaElemento/Vetor.java
@@ -0,0 +1,43 @@
+
+public class Vetor {
+    package AdicionaElemento;
+//Adicionar elemento no final do vetor
+
+public class Vetor {
+	private String[] elementos;
+	private int tamanho;
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+		this.tamanho=0;
+	}
+	
+	// ineficiente
+	public void adicionaElementoL(String elemento) {
+		for(int i=0;i<this.elementos.length;i++) {
+			if(this.elementos[i] == null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}
+	
+	// não necessário...
+	public void adicionaElementoT(String elemento) throws Exception {
+		if(this.tamanho < this.elementos.length) {
+			this.tamanho++;
+		}else {
+			throw new Exception("Vetor cheio!");
+		}
+	}
+	
+	public boolean adiciona(String elemento) {
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}
+		return false;
+	}
+
+}
