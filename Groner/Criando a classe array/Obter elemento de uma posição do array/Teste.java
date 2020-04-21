2c9bd39429953ab30b8553eaa374af055430d3ef
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-21 17:49:37 -0300

    Obter elemento de uma posição

---
+++ Groner/Criando a classe array/Obter elemento de uma posição do array/Teste.java
@@ -0,0 +1,11 @@
+
+public class Teste {
+	public static void main(String[] args) {
+		Vetor v1 = new Vetor(10);
+		v1.adiciona("1");
+		v1.adiciona("2");
+		v1.adiciona("3");
+		
+		System.out.println(v1.MostraPosicao(2));
+	}
+}
