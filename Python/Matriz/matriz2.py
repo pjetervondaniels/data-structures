0433d59b181367cfa7d9757f6074449deb9d7383
Pedro Daniel <pedro.d.jardim@hotmail.com>
2020-05-19 15:49:49 -0300

    Problema do Professor resolvido com Matriz

---
+++ Python/Matriz/matriz2.py
@@ -0,0 +1,10 @@
+matriz = [
+    ['joão', 8, 7, 6],
+    ['pedro', 4.5, 9, 10],
+    ['marcos', 6, 6, 8],
+]
+
+for linha in matriz:
+    for col in linha:
+        print(str(col) + '\t', end=' ')
+    print('')
